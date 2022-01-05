34b7967d3a8d651692e79f56df8b559f469366de
Alberus <42331357+Alberus@users.noreply.github.com>
2022-01-05 13:20:54 -0300

    API
    Estudos: teste com API

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,28 @@ function somar(){
     numAtual = numAtual + 1;
     elemento.innerHTML = numAtual
 }
+
+const BASE_URL = "https://thatcopy.pw/catapi/rest/"
+const catBtn = document.getElementById('change-cat');
+const catImg = document.getElementById('cat');
+
+
+const getImgs = async () => {
+    try {
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+    
+        return json.webpurl;
+    } catch(e) {
+        console.log(e.message)
+    }
+
+    }
+
+const loadImg = async () => {
+    catImg.src = await getImgs();
+}
+
+catBtn.addEventListener('click', loadImg)
+
+loadImg()
